f5fd5bc23201ec4ff5a096a3b135fce90fb9b45f
WerbssonSaymon <saymonluz18@gmail.com>
2023-05-23 21:18:27 -0300

    modificado o tema do blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,9 +1,9 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "CARRO ANTIGO VENDIDO POR PREÇO INACREDITAVEL",
+    "description": "Carro  antigo é vendido por um preço muito alto, veja o que disse o venderdor",
+    "photoCover":"https://tse4.mm.bing.net/th?id=OIP.RYnqLKuYnOKjoWDZbWFe4gHaEy&pid=Api&P=0&h=180"
   },
   {
     "id":"2",
